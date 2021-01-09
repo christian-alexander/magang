e20483c83239946420f3eee3c38f0025fb552df4
christian-alexander <72527589+christian-alexander@users.noreply.github.com>
2021-01-09 10:01:04 +0700

    Menampilkan Tabel Semua akun
    menampilkan tabel semua akun di halaman akun superuser, menampilkan saja, belum bisa operasi CRUD

--- magang.sql
+++ magang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2021 at 05:43 PM
+-- Generation Time: Jan 09, 2021 at 03:58 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `akun` (
-  `id` int(11) NOT NULL,
+  `id_utama` int(11) NOT NULL,
   `entitas` int(1) NOT NULL,
   `password` char(20) DEFAULT NULL,
   `email` char(50) DEFAULT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `akun` (
 -- Dumping data for table `akun`
 --
 
-INSERT INTO `akun` (`id`, `entitas`, `password`, `email`, `status`) VALUES
+INSERT INTO `akun` (`id_utama`, `entitas`, `password`, `email`, `status`) VALUES
 (1, 1, '1234', 'su1', 'on'),
 (2, 2, '1234', 'dosen1@email.com', 'on'),
 (3, 3, '1234', 'pemlap1@email.com', 'on'),
@@ -54,6 +54,7 @@ INSERT INTO `akun` (`id`, `entitas`, `password`, `email`, `status`) VALUES
 
 CREATE TABLE `dosen_pembimbing` (
   `id` int(11) NOT NULL,
+  `id_utama` int(11) NOT NULL,
   `email` char(50) NOT NULL,
   `nama` char(100) NOT NULL,
   `nidn` char(10) NOT NULL,
@@ -65,8 +66,8 @@ CREATE TABLE `dosen_pembimbing` (
 -- Dumping data for table `dosen_pembimbing`
 --
 
-INSERT INTO `dosen_pembimbing` (`id`, `email`, `nama`, `nidn`, `nowa`, `instansi`) VALUES
-(1, 'dosen1@email.com', 'Dosen1', '1014070101', '08123456789', '');
+INSERT INTO `dosen_pembimbing` (`id`, `id_utama`, `email`, `nama`, `nidn`, `nowa`, `instansi`) VALUES
+(1, 2, 'dosen1@email.com', 'Dosen1', '1014070101', '08123456789', 'im20210002');
 
 -- --------------------------------------------------------
 
@@ -76,7 +77,7 @@ INSERT INTO `dosen_pembimbing` (`id`, `email`, `nama`, `nidn`, `nowa`, `instansi
 
 CREATE TABLE `instansi` (
   `id` int(11) NOT NULL,
-  `id_usaha` char(10) NOT NULL,
+  `id_instansi` char(10) NOT NULL,
   `nama` char(100) NOT NULL,
   `notelp` char(15) NOT NULL,
   `nofax` char(15) NOT NULL,
@@ -87,9 +88,34 @@ CREATE TABLE `instansi` (
 -- Dumping data for table `instansi`
 --
 
-INSERT INTO `instansi` (`id`, `id_usaha`, `nama`, `notelp`, `nofax`, `alamat`) VALUES
+INSERT INTO `instansi` (`id`, `id_instansi`, `nama`, `notelp`, `nofax`, `alamat`) VALUES
 (1, 'im20210001', 'PT. Jaya', '03123456789', '03123456789', 'jalan buah'),
 (2, 'im20210002', 'Universitas Widya Kartika', '03123456', '03123456', 'Sutorejo Prima Utara II / 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `id_utama` int(11) NOT NULL,
+  `email` char(50) NOT NULL,
+  `nama` char(100) NOT NULL,
+  `nrp` char(8) NOT NULL,
+  `nowa` char(15) NOT NULL,
+  `dosenpembimbing` char(50) NOT NULL,
+  `pembimbinglapangan` char(50) NOT NULL,
+  `instansi` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `id_utama`, `email`, `nama`, `nrp`, `nowa`, `dosenpembimbing`, `pembimbinglapangan`, `instansi`) VALUES
+(1, 4, 'satdua86@gmail.com', 'Christian Alexander', '31119003', '082233574795', '2', '3', 'im20210001');
 
 -- --------------------------------------------------------
 
@@ -99,9 +125,10 @@ INSERT INTO `instansi` (`id`, `id_usaha`, `nama`, `notelp`, `nofax`, `alamat`) V
 
 CREATE TABLE `pembimbing_lapangan` (
   `id` int(11) NOT NULL,
+  `id_utama` int(11) NOT NULL,
   `email` char(50) NOT NULL,
   `nama` char(100) NOT NULL,
-  `nowa` char(15) DEFAULT NULL,
+  `nowa` char(15) NOT NULL,
   `instansi` char(10) NOT NULL,
   `nip` char(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -110,8 +137,8 @@ CREATE TABLE `pembimbing_lapangan` (
 -- Dumping data for table `pembimbing_lapangan`
 --
 
-INSERT INTO `pembimbing_lapangan` (`id`, `email`, `nama`, `nowa`, `instansi`, `nip`) VALUES
-(1, 'pemlap1@email.com', 'Pemlap1', '08123456789', 'im20210001', '');
+INSERT INTO `pembimbing_lapangan` (`id`, `id_utama`, `email`, `nama`, `nowa`, `instansi`, `nip`) VALUES
+(1, 3, 'pemlap1@email.com', 'Pemlap1', '08123456789', 'im20210001', '');
 
 -- --------------------------------------------------------
 
@@ -121,6 +148,7 @@ INSERT INTO `pembimbing_lapangan` (`id`, `email`, `nama`, `nowa`, `instansi`, `n
 
 CREATE TABLE `super_user` (
   `id` int(11) NOT NULL,
+  `id_utama` int(11) NOT NULL,
   `nama` char(100) NOT NULL,
   `email` char(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -129,9 +157,9 @@ CREATE TABLE `super_user` (
 -- Dumping data for table `super_user`
 --
 
-INSERT INTO `super_user` (`id`, `nama`, `email`) VALUES
-(1, 'Koordinator Magang', 'su1'),
-(2, 'Tata Usaha UWIKA', 'su2');
+INSERT INTO `super_user` (`id`, `id_utama`, `nama`, `email`) VALUES
+(1, 1, 'Koordinator Magang', 'su1'),
+(2, 5, 'Tata Usaha UWIKA', 'su2');
 
 -- --------------------------------------------------------
 
@@ -153,55 +181,8 @@ INSERT INTO `total_entitas` (`entitas`, `jumlah`) VALUES
 (2, 1),
 (3, 1),
 (4, 1),
-(5, 1),
+(5, 2),
 (9, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `total_entitas_kumulatif`
---
-
-CREATE TABLE `total_entitas_kumulatif` (
-  `entitas` int(1) NOT NULL,
-  `jumlah` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `total_entitas_kumulatif`
---
-
-INSERT INTO `total_entitas_kumulatif` (`entitas`, `jumlah`) VALUES
-(1, 2),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1),
-(9, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `email` char(50) NOT NULL,
-  `nama` char(100) NOT NULL,
-  `nrp` char(8) NOT NULL,
-  `nowa` char(15) DEFAULT NULL,
-  `dosenpembimbing` char(50) NOT NULL,
-  `pembimbinglapangan` char(50) NOT NULL,
-  `instansi` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `email`, `nama`, `nrp`, `nowa`, `dosenpembimbing`, `pembimbinglapangan`, `instansi`) VALUES
-(1, 'satdua86@gmail.com', 'Christian Alexander', '31119003', '082233574795', '1014070101', 'pemlap1', 'im20210001');
 
 --
 -- Indexes for dumped tables
@@ -211,25 +192,31 @@ INSERT INTO `user` (`id`, `email`, `nama`, `nrp`, `nowa`, `dosenpembimbing`, `pe
 -- Indexes for table `akun`
 --
 ALTER TABLE `akun`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_utama`);
 
 --
 -- Indexes for table `dosen_pembimbing`
 --
 ALTER TABLE `dosen_pembimbing`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`id_utama`);
 
 --
 -- Indexes for table `instansi`
 --
 ALTER TABLE `instansi`
-  ADD PRIMARY KEY (`id_usaha`);
+  ADD PRIMARY KEY (`id_instansi`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id_utama`);
 
 --
 -- Indexes for table `pembimbing_lapangan`
 --
 ALTER TABLE `pembimbing_lapangan`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`id_utama`);
 
 --
 -- Indexes for table `super_user`
@@ -242,18 +229,6 @@ ALTER TABLE `super_user`
 --
 ALTER TABLE `total_entitas`
   ADD PRIMARY KEY (`entitas`);
-
---
--- Indexes for table `total_entitas_kumulatif`
---
-ALTER TABLE `total_entitas_kumulatif`
-  ADD PRIMARY KEY (`entitas`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
