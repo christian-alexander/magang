af706b4e1200f8423c5c1420c2cc2445f6e9131a
christian-alexander <72527589+christian-alexander@users.noreply.github.com>
2021-01-12 17:48:12 +0700

    Bisa Buat, Edit, Delete Akun
    Sudah bisa buat edit delete akun... coming soon : buat edit delete perusahaan

--- magang.sql
+++ magang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2021 at 02:09 PM
+-- Generation Time: Jan 12, 2021 at 11:45 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -41,10 +41,10 @@ CREATE TABLE `akun` (
 
 INSERT INTO `akun` (`id_utama`, `entitas`, `password`, `email`, `status`) VALUES
 (1, 1, '1234', 'su1', 'on'),
-(2, 2, '1234', 'dosen1@email.com', 'on'),
-(3, 3, '1234', 'pemlap1@email.com', 'on'),
-(4, 4, '1234', 'satdua86@gmail.com', 'on'),
-(5, 1, '1234', 'su2', 'on');
+(2, 1, '1234', 'su2', 'on'),
+(3, 2, '1234', 'dosen1@email.com', 'on'),
+(4, 3, '1234', 'pemlap1@email.com', 'on'),
+(5, 4, '1234', 'mahasiswa1@email.com', 'on');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `dosen_pembimbing` (
   `id_utama` int(11) NOT NULL,
   `email` char(50) NOT NULL,
   `nama` char(100) NOT NULL,
-  `nidn` char(10) NOT NULL,
+  `nidn` char(50) NOT NULL,
   `nowa` char(15) NOT NULL,
   `instansi` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -67,7 +67,7 @@ CREATE TABLE `dosen_pembimbing` (
 --
 
 INSERT INTO `dosen_pembimbing` (`id`, `id_utama`, `email`, `nama`, `nidn`, `nowa`, `instansi`) VALUES
-(1, 2, 'dosen1@email.com', 'Dosen1', '1014070101', '08123456789', 'im20210002');
+(1, 3, 'dosen1@email.com', 'Dosen1', '12345678', '12345678', 'im20210002');
 
 -- --------------------------------------------------------
 
@@ -103,7 +103,7 @@ CREATE TABLE `mahasiswa` (
   `id_utama` int(11) NOT NULL,
   `email` char(50) NOT NULL,
   `nama` char(100) NOT NULL,
-  `nrp` char(8) NOT NULL,
+  `nrp` char(50) NOT NULL,
   `nowa` char(15) NOT NULL,
   `dosenpembimbing` char(50) NOT NULL,
   `pembimbinglapangan` char(50) NOT NULL,
@@ -115,7 +115,7 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `id_utama`, `email`, `nama`, `nrp`, `nowa`, `dosenpembimbing`, `pembimbinglapangan`, `instansi`) VALUES
-(1, 4, 'satdua86@gmail.com', 'Christian Alexander', '31119003', '082233574795', '2', '3', 'im20210001');
+(1, 5, 'mahasiswa1@email.com', 'Mahasiswa1', '12345678', '12345678', '', '4', 'im20210001');
 
 -- --------------------------------------------------------
 
@@ -138,7 +138,7 @@ CREATE TABLE `pembimbing_lapangan` (
 --
 
 INSERT INTO `pembimbing_lapangan` (`id`, `id_utama`, `email`, `nama`, `nowa`, `instansi`, `nip`) VALUES
-(1, 3, 'pemlap1@email.com', 'Pemlap1', '08123456789', 'im20210001', '');
+(1, 4, 'pemlap1@email.com', 'PemLap1', '12345678', 'im20210001', '');
 
 -- --------------------------------------------------------
 
@@ -159,30 +159,7 @@ CREATE TABLE `super_user` (
 
 INSERT INTO `super_user` (`id`, `id_utama`, `nama`, `email`) VALUES
 (1, 1, 'Koordinator Magang', 'su1'),
-(2, 5, 'Tata Usaha UWIKA', 'su2');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `total_entitas`
---
-
-CREATE TABLE `total_entitas` (
-  `entitas` int(1) NOT NULL,
-  `jumlah` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `total_entitas`
---
-
-INSERT INTO `total_entitas` (`entitas`, `jumlah`) VALUES
-(1, 2),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 2),
-(9, 3);
+(2, 2, 'Administrasi UWIKA', 'su2');
 
 --
 -- Indexes for dumped tables
@@ -222,13 +199,7 @@ ALTER TABLE `pembimbing_lapangan`
 -- Indexes for table `super_user`
 --
 ALTER TABLE `super_user`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `total_entitas`
---
-ALTER TABLE `total_entitas`
-  ADD PRIMARY KEY (`entitas`);
+  ADD PRIMARY KEY (`id_utama`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
