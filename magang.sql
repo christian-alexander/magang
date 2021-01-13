5777133e1617cfe560cebb5f88c2b51181ca7a96
christian-alexander <72527589+christian-alexander@users.noreply.github.com>
2021-01-13 09:58:23 +0700

    Penambahan Tabel Relasi dan Perbaikan database
    Penambahan Tabel Relasi untuk melihat relasi mahasiswa dengan dosen siapa pembimbing lapangan siapa dan perubahan minor  database

--- magang.sql
+++ magang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2021 at 11:45 AM
+-- Generation Time: Jan 13, 2021 at 03:55 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `akun` (
   `id_utama` int(11) NOT NULL,
+  `nama` char(100) NOT NULL,
   `entitas` int(1) NOT NULL,
   `password` char(200) DEFAULT NULL,
   `email` char(50) DEFAULT NULL,
@@ -39,12 +40,16 @@ CREATE TABLE `akun` (
 -- Dumping data for table `akun`
 --
 
-INSERT INTO `akun` (`id_utama`, `entitas`, `password`, `email`, `status`) VALUES
-(1, 1, '1234', 'su1', 'on'),
-(2, 1, '1234', 'su2', 'on'),
-(3, 2, '1234', 'dosen1@email.com', 'on'),
-(4, 3, '1234', 'pemlap1@email.com', 'on'),
-(5, 4, '1234', 'mahasiswa1@email.com', 'on');
+INSERT INTO `akun` (`id_utama`, `nama`, `entitas`, `password`, `email`, `status`) VALUES
+(1, 'Koordinator Magang', 1, '1234', 'su1', 'on'),
+(2, 'Administrasi UWIKA', 1, '1234', 'su2', 'on'),
+(3, 'Dosen1', 2, '1234', 'dosen1@email.com', 'on'),
+(4, 'PemLap1', 3, '1234', 'pemlap1@email.com', 'on'),
+(5, 'Mahasiswa1', 4, '1234', 'mahasiswa1@email.com', 'on'),
+(6, 'mhs2', 4, '1234', 'mhs2@email.com', 'on'),
+(7, 'mhs3', 4, '1234', 'mhs3@gmail.com', 'on'),
+(8, 'mhs4', 4, '1234', 'mhs4@email.com', 'on'),
+(9, 'mhs5', 4, '1234', 'mhs5@email.com', 'on');
 
 -- --------------------------------------------------------
 
@@ -67,7 +72,7 @@ CREATE TABLE `dosen_pembimbing` (
 --
 
 INSERT INTO `dosen_pembimbing` (`id`, `id_utama`, `email`, `nama`, `nidn`, `nowa`, `instansi`) VALUES
-(1, 3, 'dosen1@email.com', 'Dosen1', '12345678', '12345678', 'im20210002');
+(1, 3, 'dosen1@email.com', 'Dosen1', '12345678', '12345678', '2');
 
 -- --------------------------------------------------------
 
@@ -76,7 +81,6 @@ INSERT INTO `dosen_pembimbing` (`id`, `id_utama`, `email`, `nama`, `nidn`, `nowa
 --
 
 CREATE TABLE `instansi` (
-  `id` int(11) NOT NULL,
   `id_instansi` char(10) NOT NULL,
   `nama` char(100) NOT NULL,
   `notelp` char(15) NOT NULL,
@@ -88,9 +92,9 @@ CREATE TABLE `instansi` (
 -- Dumping data for table `instansi`
 --
 
-INSERT INTO `instansi` (`id`, `id_instansi`, `nama`, `notelp`, `nofax`, `alamat`) VALUES
-(1, 'im20210001', 'PT. Jaya', '03123456789', '03123456789', 'jalan buah'),
-(2, 'im20210002', 'Universitas Widya Kartika', '03123456', '03123456', 'Sutorejo Prima Utara II / 1');
+INSERT INTO `instansi` (`id_instansi`, `nama`, `notelp`, `nofax`, `alamat`) VALUES
+('1', 'PT. Jaya', '03123456789', '03123456789', 'jalan buah'),
+('2', 'Universitas Widya Kartika', '03123456', '03123456', 'Sutorejo Prima Utara II / 1');
 
 -- --------------------------------------------------------
 
@@ -115,7 +119,11 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `id_utama`, `email`, `nama`, `nrp`, `nowa`, `dosenpembimbing`, `pembimbinglapangan`, `instansi`) VALUES
-(1, 5, 'mahasiswa1@email.com', 'Mahasiswa1', '12345678', '12345678', '', '4', 'im20210001');
+(1, 5, 'mahasiswa1@email.com', 'Mahasiswa1', '12345678', '12345678', '3', '4', '1'),
+(2, 6, 'mhs2@email.com', 'mhs2', '1234', '1234', '3', '', '2'),
+(3, 7, 'mhs3@gmail.com', 'mhs3', '123', '1234', '3', '4', '1'),
+(4, 8, 'mhs4@email.com', 'mhs4', '123', '123', '3', '4', '1'),
+(5, 9, 'mhs5@email.com', 'mhs5', '1234', '1234', '3', '', '1');
 
 -- --------------------------------------------------------
 
@@ -138,7 +146,7 @@ CREATE TABLE `pembimbing_lapangan` (
 --
 
 INSERT INTO `pembimbing_lapangan` (`id`, `id_utama`, `email`, `nama`, `nowa`, `instansi`, `nip`) VALUES
-(1, 4, 'pemlap1@email.com', 'PemLap1', '12345678', 'im20210001', '');
+(1, 4, 'pemlap1@email.com', 'PemLap1', '12345678', '1', '');
 
 -- --------------------------------------------------------
 
